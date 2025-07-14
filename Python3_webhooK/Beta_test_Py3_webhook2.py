99c25e0e66eedda3c37962893744d93a7b96dc27
gogeekness <rdeseke@gmail.com>
2025-07-14 10:48:42 +0200

    debug more  adding webhook secret

--- Python3_webhooK/Beta_test_Py3_webhook2.py
+++ Python3_webhooK/Beta_test_Py3_webhook2.py
@@ -67,10 +67,10 @@ app = Flask(__name__)
 
 def verify_signature(payload_body, signature_header):
     """Verify GitHub webhook signature"""
-    
+
     if not signature_header:
         raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    hash_object = hmac.new(WEBHOOK_SECRET, msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
     if not hmac.compare_digest(expected_signature, signature_header):
         raise HTTPException(status_code=403, detail="Request signatures didn't match!")
