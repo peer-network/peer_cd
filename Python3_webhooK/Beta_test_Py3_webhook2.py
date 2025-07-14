78f93326207e03b5d552b0d0fe806eb4a897cbfc
gogeekness <rdeseke@gmail.com>
2025-07-14 11:42:46 +0200

    logger for hash

--- Python3_webhooK/Beta_test_Py3_webhook2.py
+++ Python3_webhooK/Beta_test_Py3_webhook2.py
@@ -75,6 +75,8 @@ def verify_signature(payload_body, signature_header):
         payload_body,
         hashlib.sha256
     ).hexdigest()
+    
+    hash_object = hmac.new(WEBHOOK_SECRET, msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
 
 
@@ -82,7 +84,7 @@ def verify_signature(payload_body, signature_header):
 
     if not signature_header:
         raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-    hash_object = hmac.new(WEBHOOK_SECRET, msg=payload_body, digestmod=hashlib.sha256)
+
 
     if not hmac.compare_digest(expected_signature, signature_header):
         raise HTTPException(status_code=403, detail="Request signatures didn't match!")
