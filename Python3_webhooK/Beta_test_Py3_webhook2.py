a2d16e37b0b9edde1bf56445f8da092f34ce46d8
gogeekness <rdeseke@gmail.com>
2025-07-15 14:49:11 +0200

    found an error with deploy

--- Python3_webhooK/Beta_test_Py3_webhook2.py
+++ Python3_webhooK/Beta_test_Py3_webhook2.py
@@ -280,7 +280,9 @@ def process_deployment(webhook_data):
     ## If I need to update this code (webhook)
     #  To self update the webhook
     #
-    if any(f['filename'].startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified']):
+    if any(f.startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified']):
+
+    #if any(f['filename'].startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified']):
         try:
             # Pull new code
             subprocess.run(["cp", "/opt/application/Python_webook/Python3_webhook2.py", "/home/ubuntu/myenv/peer_cd/Python3_webhook2.py"], check=True)
