ee7a6526e3b1ed773cb11d68752f5e84d3e7f85b
gogeekness <rdeseke@gmail.com>
2025-07-15 11:01:35 +0200

    Need app.run to ... run

--- Python3_webhooK/Beta_test_Py3_webhook2.py
+++ Python3_webhooK/Beta_test_Py3_webhook2.py
@@ -370,4 +370,4 @@ if __name__ == '__main__':
     logger.info(f"Target repository: {TARGET_REPO}")
     logger.info(f"Target branch: {TARGET_BRANCH}")
     logger.info(f"Local deployment directory: {LOCAL_DEPLOY_DIR}")
-    # app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
