59f5156fddd3bfe37931626548a9573f79c212fd
gogeekness <rdeseke@gmail.com>
2025-07-15 15:15:40 +0200

    typo again, chceking again

--- Python3_webhooK/Beta_test_Py3_webhook2.py
+++ Python3_webhooK/Beta_test_Py3_webhook2.py
@@ -280,8 +280,7 @@ def process_deployment(webhook_data):
     ## If I need to update this code (webhook)
     #  To self update the webhook
     #
-    #if any(f['filename'].startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified']):
-
+    #if any(f['filename'].startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified'])
 
     webhook_updated = any(f.startswith('Python3_webhook2.py') for f in webhook_data['head_commit']['modified'])
     logger.info(f"Is there webhook update: {webhook_updated}")
