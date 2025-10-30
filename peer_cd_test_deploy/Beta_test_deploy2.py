fd6cc3569e7f3f3ad1d37a5ed8a05a280f794b25
goggeekness <goggeekness@swcp.com>
2025-10-30 11:54:16 +0100

    Minor change to push to testing

--- peer_cd_test_deploy/Beta_test_deploy2.py
+++ peer_cd_test_deploy/Beta_test_deploy2.py
@@ -58,7 +58,6 @@ logger = setup_logging()
 
 ### Load configuration from JSON file
 ##  This move the configuation away from python code itself
-##
 def load_config(config_file='peer_cd_test_deploy.json'):
     import os
     try:
