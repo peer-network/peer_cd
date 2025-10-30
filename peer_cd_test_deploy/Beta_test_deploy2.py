d3f4bf053060bfedcf0ae968a370324b94b0aa0d
goggeekness <goggeekness@swcp.com>
2025-10-30 12:18:59 +0100

    minor update to test deploy

--- peer_cd_test_deploy/Beta_test_deploy2.py
+++ peer_cd_test_deploy/Beta_test_deploy2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 Post-Deployment Script for Testing and Remote Deployment
 Runs unit tests (Python, PHP, Bash) and syncs specific directories to remote servers
@@ -6,7 +7,7 @@ Tsting si good for now.
 
 This is the switchboard for DevOps, as the all (most) of the DevOps.
 So this is teh infrastructure for DevOps 
-Added (mintbot in branch) to the list added exclutions to mintbot rsync excludes
+Added (mintbot in branch) to the list added exclutions to mintbot rsync excludes.
 """
 
 import os
